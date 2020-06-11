cf053b0d00e6e6ce7744cb00992a579381e07b49
Hiraku Onuma <hiraku00@gmail.com>
2020-06-12 08:32:10 +0900

    1st commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <h1>{{ $store.state.counter }}</h1>
+    <h2>{{ cartItemsCount }}</h2> -->
+    <h1>vuex helper</h1>
+    <p>{{ message }}</p>
+    <p>{{ counter }}</p>
+    <p>{{ cartItems }}</p>
+    <p>{{ cartItemsCount }}</p>
+    <button @click="increment(1)">mutation increment</button>
+    <button @click="asyncIncrement(1)">action increment</button>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations, mapGetters, mapActions } from 'vuex'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  computed: {
+    ...mapState({
+      message: 'message',
+      counter: 'counter',
+      cartItems: 'cartItems',
+    }),
+    ...mapGetters([
+      'cartItemsCount'
+    ])
+  },
+  methods: {
+    ...mapMutations([
+      'increment'
+    ]),
+    ...mapActions({
+      asyncIncrement: 'increment'
+    })
+    // increment() {
+    //   this.$store.dispatch('increment', 10);
+    // }
   }
 }
 </script>
